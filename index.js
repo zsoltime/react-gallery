1d2558c0a15eec59c1b52189b563860654320f40
Zsolt Meszaros <zsolt@sowad.com>
2017-02-01 20:50:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,224 @@
+function handleFetchErrors(res) {
+  if (!res.ok) {
+    throw Error(res.statusText);
+  }
+  return res;
+}
+
+const Loader = ({ color, size, weight }) => {
+  const radius = size / 2;
+  const r = radius - (weight / 2);
+  const dasharray = Math.round(radius * Math.PI);
+  return (
+    <div className="loader">
+      <svg height={size} width={size}>
+        <circle
+          cx={radius}
+          cy={radius}
+          fill="none"
+          r={r}
+          stroke={color}
+          strokeLinecap="round"
+          strokeWidth={weight}
+          strokeDasharray={dasharray}
+          strokeDashoffset={radius}
+        />
+      </svg>
+    </div>
+  );
+};
+
+Loader.propTypes = {
+  size: React.PropTypes.number.isRequired,
+  color: React.PropTypes.string.isRequired,
+  weight: React.PropTypes.number.isRequired,
+};
+
+const ErrorMessage = ({ message }) => (
+  <div className="message message--error">
+    Error: {message}
+  </div>
+);
+
+ErrorMessage.propTypes = {
+  message: React.PropTypes.string.isRequired,
+};
+
+const Image = ({ className, src, alt }) => (
+  <img
+    className={className}
+    src={src}
+    alt={alt}
+  />
+);
+
+Image.propTypes = {
+  className: React.PropTypes.string.isRequired,
+  src: React.PropTypes.string.isRequired,
+  alt: React.PropTypes.string,
+};
+
+const ImageWrapper = ({ id, openImage, src, tabindex, title }) => {
+  const handleClick = () => { openImage(id); }
+
+  return (
+    <div
+      className="gallery__item"
+      role="button"
+      onClick={handleClick}
+      tabIndex={tabindex}
+    >
+      <div className="gallery__overlay">
+        <h2>{title}</h2>
+      </div>
+      <Image
+        className="gallery__image"
+        src={src}
+        alt={src}
+      />
+    </div>
+  );
+};
+
+ImageWrapper.propTypes = {
+  id: React.PropTypes.string.isRequired,
+  title: React.PropTypes.string.isRequired,
+  src: React.PropTypes.string.isRequired,
+  openImage: React.PropTypes.func.isRequired,
+};
+
+const LightBox = ({ image, closeImage }) => (
+  <div className="lightbox fade-in">
+    <div className="lightbox__wrapper">
+      <button className="lightbox__btn lightbox__btn--close" onClick={closeImage}>&times;</button>
+      <Image
+        className="lightbox__image"
+        src={image.src}
+        alt={image.desc}
+      />
+    </div>
+  </div>
+);
+
+LightBox.propTypes = {
+  image: React.PropTypes.objectOf(React.PropTypes.string).isRequired,
+  closeImage: React.PropTypes.func.isRequired,
+};
+
+const Gallery = ({ images, openImage }) => {
+  const imageWrapper = images.map(({ id, title, src }, i) => (
+    <ImageWrapper
+      key={id}
+      id={id}
+      tabindex={i + 1}
+      title={title}
+      src={src}
+      openImage={openImage}
+    />
+  ));
+  return (
+    <div className="gallery fade-in">
+      {imageWrapper}
+    </div>
+  );
+};
+
+Gallery.propTypes = {
+  images: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  openImage: React.PropTypes.func.isRequired,
+};
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      activeId: null,
+      error: null,
+      images: [],
+      isLoading: true,
+    };
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.openImage = this.openImage.bind(this);
+    this.closeImage = this.closeImage.bind(this);
+  }
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+    setTimeout(() => {
+      fetch('http://databro.com/15491/images.json')
+      .then(handleFetchErrors)
+      .then(res => res.json())
+      .then((images) => {
+        this.setState({
+          isLoading: false,
+          images,
+        });
+      })
+      .catch((err) => {
+        this.setState({
+          error: err.message,
+        });
+      });
+    }, 2e3);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+  handleKeyUp(event) {
+    if (event.which === 27 && this.state.activeId !== null) {
+      this.closeImage();
+    }
+  }
+  openImage(id) {
+    this.setState({
+      activeId: id,
+    });
+  }
+  closeImage() {
+    this.setState({
+      activeId: null,
+    });
+  }
+  render() {
+    let content;
+    const { error, isLoading, activeId, images } = this.state;
+
+    if (error) {
+      content = (
+        <ErrorMessage message={error} />
+      );
+    } else if (isLoading) {
+      content = (
+        <Loader size={200} weight={10} color="rgba(0, 0, 0, 0.2)" />
+      );
+    } else if (activeId !== null) {
+      content = (
+        <LightBox
+          image={images.filter(img => activeId === img.id)[0]}
+          closeImage={this.closeImage}
+        />
+      );
+    } else {
+      content = (
+        <Gallery
+          images={this.state.images}
+          openImage={this.openImage}
+        />
+      );
+    }
+    return (
+      <div className="app">
+        <h1 className="app__title">React Gallery</h1>
+        <header className="gallery__header">
+          <h2 className="gallery__title">Nature</h2>
+          <div className="gallery__stats">
+            <span>by @jetJake</span>
+            <span>{this.state.images.length} photos</span> <span>128 followers</span>
+          </div>
+        </header>
+        {content}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ readme.md
@@ -0,0 +1,6 @@
+# WIP React Gallery
+
+React image gallery. The app fetches a JSON file with a list of images, display
+them in a grid. Clicking on any of them opens the image in a lightbox. Instead
+of showing the images in a modal window, the app replaces the grid in the DOM
+with the lightbox.
